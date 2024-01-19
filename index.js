061c975768f12771b5c9e08df92004aebe30a8f6
Miguel dos Santos <migueldossantos0096@gmail.com>
2024-01-18 23:29:07 -0300

    adicionando dados e deletando api

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
 var DB = {
-    games:[
+    games: [
         {
             id: 23,
             title: "The legend of Zelda Breath of The Wild",
@@ -29,10 +29,51 @@ var DB = {
     ]
 }
 
-app.get("/games", (req,res) => {
+app.get("/games", (req, res) => {
     res.statusCode = 202;
     res.json(DB.games);
 });
+
+app.get("/game/:id", (req, res) => {
+    if (isNaN(req.params.id)) {
+        res.sendStatus(400);
+    } else {
+        var id = req.params.id; // Convertendo de texto para INT
+        var game = DB.games.find(g => g.id == id);
+        if (game != undefined) {
+            res.sendStatus(200);
+            res.json(game);
+        } else {
+            res.sendStatus(404);
+        }
+    }
+})
+
+app.post("/game", (req, res) => {
+    var { title, year, price } = req.body
+    DB.games.push({
+        id: 2323,
+        title,
+        price,
+        year,
+    })
+    res.sendStatus(200);
+})
+app.delete("/game/:id", (req, res) => {
+    if (isNaN(req.params.id)) {
+        res.sendStatus(400);
+    } else {
+        var id = parseInt(req.params.id); // Convertendo de texto para INT
+        var index = DB.games.findIndex(g => g.id == id);
+
+        if (index == -1) {
+            res.sendStatus(404);
+        } else {
+            DB.games.splice(index, 1);
+            res.sendStatus(200);
+        }
+    }
+})
 
 app.listen(45678, () => {
     console.log("Servidor aberto")
